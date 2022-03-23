ab9033f4f1fdbb8fdca68273eeaab3a211ad05fc
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-03-23 10:22:46 -0300

    feat: handle tasks list manipulation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks((state) => [...state, task])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks((state) => {
+      const arrayId = state.findIndex((task) => task.id === id)
+
+      return [
+        ...state.slice(0, arrayId),
+        { ...state[arrayId], done: true },
+        ...state.slice(arrayId + 1)
+      ]
+    })
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks((state) => state.filter((task) => task.id !== id))
   }
 
   return (
@@ -26,10 +40,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
