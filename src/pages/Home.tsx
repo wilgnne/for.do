54eb8756490a565659979f6c889a3c3435ee6fe5
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-05-16 11:19:00 -0300

    feat: emit alerts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,6 +9,13 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+
+    const existingTask = tasks.find(task => task.title === newTaskTitle);
+
+    if (existingTask) {
+      return Alert.alert('Task já cadastrada', 'Você não pode cadastrar uma task com o mesmo nome');
+    }
+
     const task: Task = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -31,7 +38,33 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks((state) => state.filter((task) => task.id !== id))
+    return Alert.alert(
+      'Remover item',
+      'Tem certeza que você deseja remover esse item?',
+      [
+        {
+          text: 'Não',
+          style: 'cancel',
+        },
+        {
+          text: 'Sim',
+          onPress: () => {
+            setTasks((state) => state.filter((task) => task.id !== id))
+          }
+        },
+      ])
+  }
+
+  function handleEditTask(taskId: number, taskNewTitle: string) {
+    setTasks((state) => {
+      const arrayId = state.findIndex((task) => task.id === taskId)
+
+      return [
+        ...state.slice(0, arrayId),
+        { ...state[arrayId], title: taskNewTitle },
+        ...state.slice(arrayId + 1)
+      ]
+    })
   }
 
   return (
@@ -44,6 +77,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   )
